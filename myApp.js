e3582276feba12fc10664efaa2d8a19b3a7d8f7e
Chryssa Karatza <chryssakaratza@gmail.com>
2023-04-11 16:36:20 +0100

    implement root-level request logger middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,13 @@ let app = express();
 
 //console.log("Hello World");
 
+const myLogger = function(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+}
+
+app.use(myLogger);
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
@@ -21,15 +28,6 @@ app.get("/json", (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-  
 
 
 
