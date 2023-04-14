a7ffc0bd3515164d30f782c4c41543c90bc81aca
Chryssa Karatza <chryssakaratza@gmail.com>
2023-04-14 16:18:26 +0100

    chain middleware

--- myApp.js
+++ myApp.js
@@ -26,7 +26,12 @@ app.get("/json", (req, res) => {
     res.json({"message": message});
 });
 
-
+app.get("/now", function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+});
 
 
 
